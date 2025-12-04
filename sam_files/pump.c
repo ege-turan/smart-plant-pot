8a176645a8b8f9c78b5bd4f77ed63f07f2e52db7
Sam Chen <samchen2@stanford.edu>
2025-12-03 21:11:06 -0800

    added pump files

---
+++ sam_files/pump.c
@@ -0,0 +1,27 @@
+/*
+ * pump.c
+ *
+ *  Created on: Dec 3, 2025
+ *      Author: shuanlinchen
+ */
+
+#include "pump.h"
+
+void pump_init(void) {
+	// drive GPIO PB2 high, drive pump high for some period of time first (based on tubing length) to fill the tubing
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
+	HAL_Delay(1000);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+
+}
+
+void pump_on(void) {
+	// drive GPIO PB2 low
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
+}
+
+void pump_off(void) {
+	// drive GPIO PB2 low
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+}
+
